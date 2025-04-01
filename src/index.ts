f0bf5b848645c1c994f68685cc24576e7f92943a
malware171 <henrique.2004mauro@outlook.com>
2025-04-01 15:42:50 -0300

    Tentativa de resolução do terceiro exercicio

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ function multiply() {
   let b = 1000;
 
   let soma = a + b;
-  let result: number;
+  let result;
 
   for (let i = b; i <= soma; i++) {
     if (i % a === 0) {
       result = i;
-      console.log(result);
     }
   }
+  console.log("O Próximo Múltiplo de " + b + " é: " + result);
 }
-multiply();
+//multiply();
 
 // dia - mes - ano
 // 1 - 3 - 25
@@ -22,3 +22,49 @@ function validade() {
   const mes = 3;
   const ano = 2025;
 }
+
+function isNumberPrimo() {
+  let num = 3;
+
+  if (num <= 1) {
+    return console.log("O numero " + num + " não é primo");
+  } else if (num === 2) {
+    return console.log("O numero " + num + " é primo");
+  } else {
+    for (let i = 2; i <= num - 1; i++) {
+      if (num % i === 0) {
+        return console.log("O numero " + num + " é primo");
+      } else {
+        return console.log("O numero " + num + " não é primo");
+      }
+    }
+  }
+
+  /* 
+  else if (num === 2) {
+    isPrimo = true;
+  } else if (num > 2 && num % 2 === 0) {
+    for (let i = 2; i <= num - 1; i++) {
+      if (num % i === 0) {
+        isPrimo = true;
+        break;
+      } else {
+        isPrimo = false;
+        break;
+      }
+    }
+  }*/
+
+  /*
+  else if (num === 2) {
+    return console.log(console.log("O numero " + num + " é primo"));
+  } else {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if ((i * num) % 2 === 0) {
+        return console.log("O numero " + num + " é primo");
+      }
+    }
+    return console.log("O numero " + num + " não é primo");
+  }*/
+}
+isNumberPrimo();
