51ed2294aa75686468dea8623b37d14af8a79eaf
Malware171 <henrique.2004mauro@outlook.com>
2025-04-02 10:15:29 -0300

    Otimização no codigo

--- dist/index.js
+++ dist/index.js
@@ -19,48 +19,20 @@ function validade() {
     const mes = 3;
     const ano = 2025;
 }
-function isNumberPrimo() {
-    let num = 3;
+function isNumberPrimo(num) {
     if (num <= 1) {
-        return console.log("O numero " + num + " não é primo");
-    }
-    else if (num === 2) {
-        return console.log("O numero " + num + " é primo");
+        console.log("Nao é primo");
     }
     else {
         for (let i = 2; i <= num - 1; i++) {
-            if (num % i === 0) {
-                return console.log("O numero " + num + " é primo");
+            if ((i * num) % 2 === 0) {
             }
-            else {
-                return console.log("O numero " + num + " não é primo");
-            }
+            return console.log("Nao é primo");
         }
+        return console.log("É primo");
     }
-    /*
-    else if (num === 2) {
-      isPrimo = true;
-    } else if (num > 2 && num % 2 === 0) {
-      for (let i = 2; i <= num - 1; i++) {
-        if (num % i === 0) {
-          isPrimo = true;
-          break;
-        } else {
-          isPrimo = false;
-          break;
-        }
-      }
-    }*/
-    /*
-    else if (num === 2) {
-      return console.log(console.log("O numero " + num + " é primo"));
-    } else {
-      for (let i = 2; i <= Math.sqrt(num); i++) {
-        if ((i * num) % 2 === 0) {
-          return console.log("O numero " + num + " é primo");
-        }
-      }
-      return console.log("O numero " + num + " não é primo");
-    }*/
+    // SE ENCONTRAR ALGUM DIVISOR QUE ESTEJA NO INTERVALO DE (2 e n-1) E O RESTO FOR = 0.
+    // O NUMERO NÃO É PRIMO.
+    /// SE NÃO TIVER NENHUM DIVISOR ENTRE O NUMERO - 1 QUE NÃO SEJA 1 OU ELE MESMO O NUMERO É PRIMO
 }
-isNumberPrimo();
+isNumberPrimo(3);
