46980830d82441e7c18190249203579961f036cb
Malware171 <henrique.2004mauro@outlook.com>
2025-04-02 13:41:53 -0300

    Exercicio 03 corrigido

--- src/index.ts
+++ src/index.ts
@@ -17,46 +17,30 @@ function multiply() {
 // dia - mes - ano
 // 1 - 3 - 25
 
-function validity() {
-  const dateToday = {
-    dayToday: 2,
-    monthToday: 4,
-    yearToday: 2025,
-  };
-
-  let productValidity = {
-    product: "Chocolate",
-    day: 1,
-    month: 4,
-    year: 2025,
-  };
-
-  if (productValidity.year < dateToday.yearToday) {
-    console.log("Expired product");
-  } else if (productValidity.month < dateToday.monthToday) {
-    console.log("Expired product");
-  } else if (productValidity.day < dateToday.dayToday) {
-    console.log("Expired product");
-  } else {
-    console.log("Product is not Experied");
-  }
+function validade() {
+  const dia = 1;
+  const mes = 3;
+  const ano = 2025;
 }
-validity();
 
 function isNumberPrimo(num: number) {
   if (num <= 1) {
     console.log("Nao é primo");
+  } else if (num === 2) {
+    return console.log("É primo");
+  } else if (num % 2 === 0 && num > 2) {
+    console.log("Nao é primo");
   } else {
-    for (let i = 2; i <= num - 1; i++) {
-      if ((i * num) % 2 === 0) {
+    for (let i = 2; i < num - 1; i++) {
+      if (num % i === 0) {
+        return console.log("Nao é primo");
       }
-      return console.log("Nao é primo");
     }
     return console.log("É primo");
   }
-
-  // SE ENCONTRAR ALGUM DIVISOR QUE ESTEJA NO INTERVALO DE (2 e n-1) E O RESTO FOR = 0.
-  // O NUMERO NÃO É PRIMO.
-  /// SE NÃO TIVER NENHUM DIVISOR ENTRE O NUMERO - 1 QUE NÃO SEJA 1 OU ELE MESMO O NUMERO É PRIMO
 }
-//isNumberPrimo(3);
+// SE ENCONTRAR ALGUM DIVISOR QUE ESTEJA NO INTERVALO DE (2 e n-1) E O RESTO FOR = 0.
+// O NUMERO NÃO É PRIMO.
+/// SE NÃO TIVER NENHUM DIVISOR ENTRE O NUMERO - 1 QUE NÃO SEJA 1 OU ELE MESMO O NUMERO É PRIMO
+
+isNumberPrimo(161);
